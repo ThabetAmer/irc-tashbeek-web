2eb88f9dc7d6a78ee5165eda5fbdadab4c9def68
Solaiman  Kmail <psokmail@gmail.com>
2018-12-19 09:59:45 +0200

    Add JobOpening fields

--- app/Sync/Cases/JobOpening.php
+++ app/Sync/Cases/JobOpening.php
@@ -12,7 +12,8 @@ class JobOpening extends AbstractCase
      */
     public function id(): string
     {
-        return 'c8e84a46588b5001be2e593f45034f20a1ff35c2';
+        // Job opening belongs to firm
+        return 'e2a4b4924d5675493681b3f3ce63c4ed94430d8e';
     }
 
     /**
@@ -23,18 +24,32 @@ class JobOpening extends AbstractCase
     public function questions(): array
     {
         return [
-            'date_required' => [
-                'name' => 'date_required',
-                'type' => 'date'
-            ],
             'job_id' => [
                 'name' => 'job_id',
+                'type' => 'string'
+            ],
+            'vtit' => [
+                'name' => 'job_title',
                 'type' => 'text'
             ],
-            'why_close_job_opening' => [
-                'name' => 'why_close_job_opening',
+            'vdesc' => [
+                'name' => 'job_description',
                 'type' => 'text'
             ],
+            'coworkers_gender' => [
+                'name' => 'gender',
+                'type' => 'text'
+            ],
+            'coworker_nationality' => [
+                'name' => 'nationality',
+                'type' => 'text'
+            ]
+        ];
+    }
+    public function formId()
+    {
+        return [
+            '7c99a14775e998cba17c353a26de887715e32b74'
         ];
     }
 }

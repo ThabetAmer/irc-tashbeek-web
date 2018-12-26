9ea5b7bcb50363b33f8ba6583dd839e8ebcffdc8
mohammedsehweil <sehweil@souktel.org>
2018-12-26 16:29:34 +0200

    Fix .

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -10,12 +10,14 @@
     <div class="user-section fhd:pl-24 hd:pl-16 pl-16 text-white mb-6 relative mb-3">
         <div class="text-2xl fhd:text-3xl hd:text-xl text-bold mb-2 hide-min ">Mike Nolan</div>
         <div class="text-grey-dark text-base hide-min">Location: Amman</div>
-        <img class="rounded-full absolute pin-l pin-t mt-3 hd:mt-2 ml-1" src="https://picsum.photos/40/40" alt="">
+        <img class="rounded-full absolute pin-l pin-t mt-3 hd:mt-2 ml-1 w-12"
+             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC8dqjys69YAcJBCAsXrwVAkdDMI6p3r_xIRRt-XDhuZyp7gNd"
+             alt="">
     </div>
 
     <ul class="list-unstyled components list-reset">
 
-        <li class="uppercase  text-white {{ Request::url() == url('/dashboard') ?'active' :''}} " >
+        <li class="uppercase  text-white {{ Request::url() == url('/dashboard') ?'active' :''}} ">
             <a href="/dashboard" class="text-white flex items-center  text-sm remove-text-minified py-4 relative mb-3">
                 <i class="icon-Presentation_3_1 min-w-30 text-2xl mr-10 ml-3"></i>
                 <span>
@@ -50,14 +52,14 @@
         </li>
 
         <li class="uppercase  text-white {{ Request::url() == url('/job-openings') ?'active' :''}}">
-            <a href="/job-openings"  class="text-white flex items-center  text-sm remove-text-minified py-4 relative mb-3">
+            <a href="/job-openings"
+               class="text-white flex items-center  text-sm remove-text-minified py-4 relative mb-3">
                 <i class="icon-Briefcase_x40_2xpng_2 min-w-30  pin-l pin-t text-xl mr-10 ml-3"></i>
                 <span>
                     Jobs
                 </span>
             </a>
         </li>
-
 
 
         <li class="uppercase  text-white">
@@ -80,6 +82,12 @@
     </ul>
 </nav>
 
+<style>
+    .hd\:mt-2 {
+        margin-top: -0.5rem;
+    }
+
+</style>
 
 @section('script')
 

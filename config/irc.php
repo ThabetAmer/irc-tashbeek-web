c193a21ad12e711ef3610b36f1e5d7bbd22375eb
mohammedsehweil <sehweil@souktel.org>
2019-01-22 15:50:00 +0200

    password validation (forgot password + user creation + on update )

--- config/irc.php
+++ config/irc.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-  'commcare_api_key' => 'issa@souktel.org:f9fcd4b96a0d632d7195b42413417c8ba61a5f5b'
+    'commcare_api_key' => 'issa@souktel.org:f9fcd4b96a0d632d7195b42413417c8ba61a5f5b',
+    'password_regex_validation' => '/^.*(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d].*$/',
 ];

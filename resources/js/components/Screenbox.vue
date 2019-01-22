7c3673ce11a9bfd22dce9d74c163625bd2d7ca4a
Mahmoud <mahmoud.abdalkarim@souktel.org>
2019-01-22 15:54:59 +0200

    screening done

--- resources/js/components/Screenbox.vue
+++ resources/js/components/Screenbox.vue
@@ -1,21 +1,16 @@
 <template>
   <!--
     -->
-  <div :class="' notebox p-8 text-left  border-solid  border-grey-light relative mb-3 '+' '+`${customClass}`">
+  <div :class="' notebox p-8 text-left  border  border-grey-light relative mb-3 '+' '+`${customClass}`">
     <div class=" text-grey-dark text-sm mb-1">
-      WEDNESDAY 12 NOVEMBER
+      {{screen.created_at_text}}
     </div>
 
     <div class="mb-4 text-base text-black font-bold ">
-      Candidate screened for a job ID 0109 with Souktel Inc.
+      {{screen.title}}
     </div>
-    <div class=" text-sm text-black font-semibold">
-      Lorem Ipsum is simply dummy text of the printing
-      and typesetting industry. Lorem Ipsum has been the industry's
-      standard dummy text ever since the 1500s, when an unknown printer
-      took a galley of type and scrambled it to make a type specimen book.
-      It has survived not only five centuries, but also the leap into electronic
-      typesetting, remaining essentially unchanged
+    <div v-if="screen.note" class=" text-sm text-black font-semibold">
+      {{screen.note}}
     </div>
   </div>
 </template>
@@ -23,20 +18,26 @@
 
 <script>
 
-    export default {
-        mixins: [],
-        /**
-         * all props have their needed types
-         * and are passed using the mixin
-         */
-        props: {
-
-            customClass:{
-                type:[String],
-                default:'border'
-            }
+  export default {
+    mixins: [],
+    /**
+     * all props have their needed types
+     * and are passed using the mixin
+     */
+    props: {
+      screen: {
+        type: Object,
+        default: () => {
+          {
+          }
         }
+      },
+      customClass: {
+        type: String,
+        default: ''
+      }
     }
+  }
 </script>
 
 <style lang="scss">

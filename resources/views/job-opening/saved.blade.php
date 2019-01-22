181d37740c747322613667528a79aa532f905095
Mahmoud <mahmoud.abdalkarim@souktel.org>
2019-01-22 13:46:00 +0200

    matches and saved matches breadcrumbs

--- resources/views/job-opening/saved.blade.php
+++ resources/views/job-opening/saved.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.layout')
 @section('content')
     <div>
+        <breadcrumbs
+                :crumbs="[{name:'{{trans('irc.job_openings')}}', link:'{{route('job-openings')}}'},
+                {name:'{{ trans('irc.job_opening_saved_match') }} [ {{$jobOpening->job_title}} ]', link:''}]"
+        ></breadcrumbs>
         <job-opening-saved-matches
             route="{{route('api.matches.saved',[$jobOpening->id])}}"
             matches-url="{{route('job-openings.match',[$jobOpening->id])}}"

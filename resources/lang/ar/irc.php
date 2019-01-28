d8aa4f131183a201d8f0ae0fb1ff7acedcc8be58
Solaiman  Kmail <psokmail@gmail.com>
2019-01-28 13:50:50 +0200

    Fix translation

--- resources/lang/ar/irc.php
+++ resources/lang/ar/irc.php
@@ -19,7 +19,7 @@ return [
     'total_job_seekers_monthly_intakes' => 'مدخلات الباحثين عن عمل لهذا الشهر',
     'total_pending_followups' => 'متابعات معلقة',
     'total_job_openings' => 'الوظائف المفتوحة',
-    'total_firms_per_user' => 'عدد مدخلات اصحاب العمل لهذا الشهر',
+    'total_firms_per_user' => 'عدد مدخلات اصحاب العمل',
     'followups' => 'المتابعات',
     'recent_activity' => 'اخر النشاطات',
     'no_data_to_present' => 'لا يوجد بيانات لعرضها.',
